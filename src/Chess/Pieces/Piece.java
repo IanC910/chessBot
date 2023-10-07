6aaa02971326464a29c05e056989c8689001ac69
Ian <thecruik@gmail.com>
2023-10-07 15:19:44 -0700

    Add Static Piece Store in Piece.java

--- src/Chess/Pieces/Piece.java
+++ src/Chess/Pieces/Piece.java
@@ -7,6 +7,28 @@ import Chess.Position;
 
 public abstract class Piece {
 
+    // Static Piece Store
+
+    public static final Piece NO_PIECE      = new NoPiece();
+
+    public static final Piece WHITE_PAWN    = new Pawn      (Colour.WHITE);
+    public static final Piece WHITE_BISHOP  = new Bishop    (Colour.WHITE);
+    public static final Piece WHITE_KNIGHT  = new Knight    (Colour.WHITE);
+    public static final Piece WHITE_ROOK    = new Rook      (Colour.WHITE);
+    public static final Piece WHITE_QUEEN   = new Queen     (Colour.WHITE);
+    public static final Piece WHITE_KING    = new King      (Colour.WHITE);
+
+    public static final Piece BLACK_PAWN    = new Pawn      (Colour.BLACK);
+    public static final Piece BLACK_BISHOP  = new Bishop    (Colour.BLACK);
+    public static final Piece BLACK_KNIGHT  = new Knight    (Colour.BLACK);
+    public static final Piece BLACK_ROOK    = new Rook      (Colour.BLACK);
+    public static final Piece BLACK_QUEEN   = new Queen     (Colour.BLACK);
+    public static final Piece BLACK_KING    = new King      (Colour.BLACK);
+
+
+
+    // Non-Static Members
+
     public enum Colour {
         NONE  (' '),
         WHITE ('w'),
@@ -22,13 +44,6 @@ public abstract class Piece {
     public final Colour colour;
     public final char symbol;
     public final int value;
-    
-    // public static final Piece NO_PIECE = new NullPiece();
-
-    // public static final Piece WHITE_PAWN = new Pawn(Colour.WHITE);
-    // public static final Piece BLACK_PAWN = new Pawn(Colour.BLACK);
-
-    // TODO static constants for each piece (maybe in Piece Catalogue)
 
     public Piece(Colour colour, char symbol, int value) {
         this.colour = colour;
