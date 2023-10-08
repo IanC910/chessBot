3925a3a5dd731f220f4f750682323be14ee8a16b
Ian <thecruik@gmail.com>
2023-10-08 00:59:04 -0700

    Add guts to Position.java

--- src/Chess/Position.java
+++ src/Chess/Position.java
@@ -1,5 +1,16 @@
 package Chess;
 
 public class Position {
-    
+
+    public int rank;
+    public int file;
+
+    public Position(int rank, int file) {
+        this.rank = rank;
+        this.file = file;
+    }
+
+    public boolean equals(Position position) {
+        return(this.rank == position.rank && this.file == position.file);
+    }
 }
