3aaafb7d4df6f04f91db8b5699f0bff36514fd84
Ian <thecruik@gmail.com>
2023-10-08 01:45:53 -0700

    Make Position and Move attributes final

--- src/Chess/Move.java
+++ src/Chess/Move.java
@@ -4,15 +4,20 @@ import Chess.Pieces.Piece;
 
 public class Move {
 
-    public Position startPos;
-    public Position endPos;
+    public final Position startPos;
+    public final Position endPos;
 
     public Move(int startRank, int startFile, int endRank, int endFile) {
         this.startPos = new Position(startRank, startFile);
         this.endPos = new Position(endRank, endFile);
     }
 
-    public boolean doesCheckOwnKing(Board board) {
+    public Move(Position startPos, Position endPos) {
+        this.startPos = startPos;
+        this.endPos = endPos;
+    }
+
+    public boolean checksOwnKing(Board board) {
         Board newBoard = board.clone();
 
         Piece piece = newBoard.getPiece(this.startPos);
--- src/Chess/Position.java
+++ src/Chess/Position.java
@@ -2,8 +2,21 @@ package Chess;
 
 public class Position {
 
-    public int rank;
-    public int file;
+    public static boolean isValid(int rank, int file) {
+        return (
+            rank >= Board.MIN_RANK &&
+            rank <= Board.MAX_RANK &&
+            file >= Board.MIN_FILE &&
+            file <= Board.MAX_FILE
+        );
+    }
+
+    public static boolean isValid(Position position) {
+        return isValid(position.rank, position.file);
+    }
+
+    public final int rank;
+    public final int file;
 
     public Position(int rank, int file) {
         this.rank = rank;
@@ -12,5 +25,9 @@ public class Position {
 
     public boolean equals(Position position) {
         return(this.rank == position.rank && this.file == position.file);
+    }
+
+    public boolean isValid() {
+        return isValid(this.rank, this.file);
     }
 }
