b1eb006cad726017bf3ea34bab2748534fa22fe0
IanC910 <ian.cruikshank.01@gmail.com>
2025-02-09 16:12:40 -0800

    Add Board::hasInsufficientMaterial()

--- chessTests/BoardTests.cpp
+++ chessTests/BoardTests.cpp
@@ -39,5 +39,38 @@ namespace BoardTests {
 			board.setPiece(4, 4, Piece(WHITE, PAWN));
 			Assert::IsTrue(board.equals(copy));
 		}
+
+        TEST_METHOD(materialValueTest) {
+            Board board;
+            board.reset();
+            Assert::IsTrue(0 == board.getMaterialValue());
+
+            board.setPiece(0, 0, Piece::NO_PIECE);
+            Assert::IsTrue(-5 == board.getMaterialValue());
+        }
+
+        TEST_METHOD(insufficientMaterialTest) {
+            Board board;
+
+            board.setPiece(0, 0, {WHITE, KING});
+            board.setPiece(1, 0, {WHITE, PAWN});
+            board.setPiece(7, 7, {BLACK, KING});
+            Assert::IsFalse(board.hasInsufficientMaterial());
+
+            board.setPiece(1, 0, {WHITE, BISHOP});
+            Assert::IsTrue(board.hasInsufficientMaterial());
+
+            board.setPiece(1, 0, Piece::NO_PIECE);
+            Assert::IsTrue(board.hasInsufficientMaterial());
+
+            board.setPiece(6, 7, {BLACK, KNIGHT});
+            Assert::IsTrue(board.hasInsufficientMaterial());
+
+            board.setPiece(2, 2, {WHITE, KNIGHT});
+            Assert::IsTrue(board.hasInsufficientMaterial());
+
+            board.setPiece(1, 2, {WHITE, KNIGHT});
+            Assert::IsFalse(board.hasInsufficientMaterial());
+        }
     };
 }
