ec11af87262e6501858099180b8ef32084c89536
IanC910 <ian.cruikshank.01@gmail.com>
2025-02-09 16:45:21 -0800

    Create botDescriptions.hpp

---
+++ chess/bots/botDescriptions.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <string>
+
+const std::string RANDALL_BOT_NAME = "Randall";
+const std::string RANDALL_BOT_DESCRIPTION = "Knows how the pieces move, but that's about it. Makes random moves.";
+
+const std::string GREEDO_BOT_NAME = "Greedo";
+const std::string GREEDO_BOT_DESCRIPTION = "Greedy little bastard, and quite stupid. Random but prioritizes taking pieces.";
